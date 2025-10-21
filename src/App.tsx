969eb6a07b6a23a821f252b1bdf7f4bf5171b20c
Tetiana Vozniuk <taniavoznyk19655@gmail.com>
2025-10-21 18:10:43 +0300

    Prepare for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,16 @@ import "./App.css";
 import { Dashboard } from "./components/Dashboard/Dashboard";
 import { Header } from "./components/Header/Header";
 import wishes from "../db.json";
-import { Route, Routes } from "react-router-dom";
+// import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
 
-      <Routes>
-
-        <Route path="/" element={<Dashboard wishes={wishes.wishes} />} />
-      </Routes>
-      
+      {/* <Routes> */}
+        <Dashboard wishes={wishes.wishes} />
+      {/* </Routes> */}
     </>
   );
 }
